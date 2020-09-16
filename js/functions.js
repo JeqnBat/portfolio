9f6b55193c1f707d727cf7da7997418ac3d8513f
Jean-Baptiste <46994366+JeqnBat@users.noreply.github.com>
2020-09-16 22:15:56 +0200

    daily commit

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,17 @@
 $(function() {
-  let move = new Animations()
-  let events = new Events(move)
+  let view = new View()
+  let model = new Model(view)
+  let controller = new Controller(model)
+  let project = []
 
-  events.titleClick()
+  for (let i = 0; i < data.length; i++) {
+    project[i] = new Project(data[i].title, i+2, data[i].descr, data[i].img, data[i].date, data[i].color, techIcons, data[i].resp, data[i].github)
+    view.printMiniature(project[i])
+    controller.miniMouseOver(project[i])
+  }
+
+  console.log(project)
+
+  controller.titleClick()
+  controller.langClick()
 })
